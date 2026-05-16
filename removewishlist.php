bf9804dc715225a49c52288b9bc14e97ddf972c8
Reezhr <reeshahr796@gmail.com>
2026-05-16 11:49:06 +0530

    Handle AJAX request for wishlist removal

--- removewishlist.php
+++ removewishlist.php
@@ -1,7 +1,14 @@
 <?php
 include "config.php";
+// Detect AJAX
+$isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
 
 if (!isset($_SESSION['user_id'])) {
+    if ($isAjax) {
+        header('Content-Type: application/json');
+        echo json_encode(['status' => 'login_required']);
+        exit();
+    }
     header("Location: booklogin.php");
     exit();
 }
@@ -11,6 +18,12 @@ if (isset($_GET['id'])) {
     $book_id = (int)$_GET['id'];
 
     mysqli_query($conn, "DELETE FROM wishlist WHERE user_id='$user_id' AND book_id='$book_id'");
+
+    if ($isAjax) {
+        header('Content-Type: application/json');
+        echo json_encode(['status' => 'ok', 'removed' => true, 'book_id' => $book_id]);
+        exit();
+    }
 }
 
 header("Location: bookwishlist.php");
